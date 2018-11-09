7df2a48946e0d3634918450d244ae64ce3efbc5a
島川徹平 <shimakawateppei@sitetsuMac.local>
2018-11-09 10:00:59 +0900

    181109

--- js/main.js
+++ js/main.js
@@ -1,24 +1,37 @@
 
 
-
-var dataArray;
 var result="";
 var result2="";
-var requestId;
-//var sixteenArray=[];
+var cost="";
+var costArray=[];
 
+baseArray=[
+0000000000000000,5554211100000000,5554211100000000,5554211110000000,
+5554211000000000,5553110000000000,4442000000000000,1110000000000000
+];
 
-document.getElementById('button').addEventListener('click',function(){
+console.log(baseArray[0].toString());
 
+document.getElementById('button').addEventListener('click',function()
+{
+     navigator.getUserMedia = navigator.getUserMedia
+     || navigator.webkitGetUserMedia
+     || navigator.mozGetUserMedia;
+     window.AudioContext = window.AudioContext
+     || window.webkitAudioContext
+     || window.mozAudioContext;
 
-//audio setup
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-var analyser = audioCtx.createAnalyser();
-analyser.fftSize = 32;
-analyser.smoothingTimeConstant=0.8;
-var bufferLength=analyser.frequencyBinCount; //fftの1/2
-dataArray=new Uint8Array(bufferLength); //16個の0-255値
-analyser.getByteFrequencyData(dataArray);
+ navigator.getUserMedia({audio:true}, function(stream)
+  {
+       var audioCtx = new AudioContext();
+       var source = audioCtx.createMediaStreamSource(stream);
+       var analyser = audioCtx.createAnalyser();
+       analyser.fftSize = 32;
+       analyser.smoothingTimeConstant=0.8;
+       source.connect(analyser);
+       var bufferLength=analyser.frequencyBinCount; //fftの1/2
+       var dataArray=new Uint8Array(bufferLength); //16個の0-255値
+       //analyser.connect(audioCtx.destination);
 
 //canvas setup
 var canvas=document.getElementById("canvas");
@@ -26,99 +39,139 @@ var ctx=canvas.getContext("2d");
 canvas.setAttribute('width', analyser.frequencyBinCount * 10); //fftの1/2*10
 canvas.setAttribute('height', window.innerHeight/5);
 
-
-
-// ファイルを取得 (arraybufferとして)
-var request = new XMLHttpRequest();
-request.open('GET', './deer.mp3', true);
-request.responseType = 'arraybuffer'; //textでなくarray bufferとしてload
-
-request.onload = function ()
+render();
+  //render()後4.2sでendが発火
+function end()
 {
-    //bufferをanalyserに渡すsourceコンテキストを生成
-  var source=audioCtx.createBufferSource();
-    // 読み込みが終わったら、decodeしてbufferにいれておく
-    audioCtx.decodeAudioData(request.response, function (buf)
-    {
-
-
-    source.buffer=buf;
-    analyser.connect(audioCtx.destination);
-    source.connect(analyser);
-    source.start();
-    console.log(buf);
-    });
-};
-request.send();
+cancelAnimationFrame(requestId);
+}
+setTimeout(end,4200);
 
 
 function render()
- {
+{
 //analyserに入っている周波数成分をArrayに渡す
  analyser.getByteFrequencyData(dataArray);
  ctx.clearRect(0,0,canvas.width,canvas.height)
 
  for(i=0;i<dataArray.length;i++)
-  {
+   {
   ctx.fillRect(i*10,0,2,dataArray[i]/2)
+  //数値を小さく加工
   result += Math.round(dataArray[i]/50);
-  }
+   }
   // \B:単語の境界(空白、カンマ、ピリオド)以外の位置 (?=aaa):aaaの直前の位置
   //(?!aaa):aaaではない直前の位置。カンマ挿入された後も正常処理できるよう。
   //g:繰り返しオプション
   result=result.replace(/\B(?=(\d{16})+(?!\d))/g,',');
+  //解析結果の配列
   result=result.split(",");
-  console.log(result.length/30)
 
   var final='';
   var oriArray='';
-  for(i=0;i<result.length;i=i+30)
-    {
+  for(i=30;i<result.length;i=i+30)
+{
      oriArray=result[i];
-     final += result[i]
-    };
-  final=final.replace(/\B(?=(\d{16})+(?!\d))/g,',');
-    //result += result[i]
-  requestId=requestAnimationFrame(render);
+     final += result[i];
+  //for leven
+  var a=result[i].toString();
 
-  console.log(oriArray);
+  var kekka=final.replace(/\B(?=(\d{16})+(?!\d))/g,',');
+  document.getElementById("txt").value=kekka;
 
-  /*for(i=0;i<20;i++){
-　sixteenArray=result.slice(i*16,(i+1)*15);
+}
+ for(i=1;i<baseArray.length;i++)
+  {
+  var b=baseArray[i].toString();
   }
-  var cnt=0;
-  cnt ++;
-  if(cnt%2 == 0){
-  result2 += sixteenArray[cnt];
-  };*/
-  document.getElementById("txt").value=final;
- }
+ requestId=requestAnimationFrame(render);
+ cost = levenshteinDistance(a,b);
+console.log(cost);
+document.getElementById("cost").value=cost;
+//leven start
 
-render();
+cost = levenshteinDistance(a,b);
+console.log(cost);
+document.getElementById("cost").value=cost;
 
-  //render()後4.2sでendが発火
-  function end(){
-  cancelAnimationFrame(requestId);
+
+function levenshteinDistance( str1, str2 )
+{
+  if(str1)
+  {
+var x = str1.length;
   }
-  setTimeout(end,4200);
+  if(str2)
+  {
+var y = str2.length;
+  }
+var d = [];  //距離コスト値
 
+//縦横の１列目は空文字との比較なのでコストは文字数と同じ
+for(var e=0; e<=x; e++)
+　{
+  d[e] = [];
+  d[e][0] = e;
+  }
+
+for(var e=0; e<=y; e++)
+　{
+  d[0][e] = e;
+  }
+
+//以下はd[1][1]より大きいマスを埋めるための記述
+var cost = 0;
+for(var e=1; e<=x; e++)
+　{
+  for(var j=1; j<=y; j++)
+  　{
+    //斜めの文字が同一だったらコスト+0
+    cost = str1[e-1] == str2[j-1] ? 0 : 1;
+    //costは上+1、左+1、左上+costの一番小さいものになる。
+　　d[e][j] = Math.min(d[e - 1][j] +1, d[e][j - 1] +1, d[e - 1][j - 1] + cost);
+    }
+  }
+
+/*
+    // 表の作成開始
+    var rows=[];
+    var table = document.createElement("table");
+    table.border="1"
+    // 表に2次元配列の要素を格納
+    for(e = 0; e <= x; e++)
+    {
+     rows.push(table.insertRow(-1));  // 行の追加
+     for(j = 0; j <= y; j++)
+     {
+     cell=rows[e].insertCell(-1);
+     cell.appendChild(document.createTextNode(d[e][j]));
+     }
+    }
+    // 指定したdiv要素に表を加える
+    document.getElementById("tbl").appendChild(table);
+*/
+    
+      return d[x][y];
+  };
+
+
+
+
+
+//leven end
+
+
+
+
+
+  //for render ()
+  }
+  //for navigator ()
+ }, function()
+      {
+       alert('error');
+      });
+  //for click ()
 });
 
 
-
-/*
-var bufferLength;
-var frequency;
-var value;
-analyser.fftSize = 2048;                    // デフォルト2048
-bufferLength = analyser.frequencyBinCount;  // analyser.fftSizeの半分のため、1024
-frequency = new Uint8Array(bufferLength); //1024個の0-255値の数字配列
-
-analyser.getByteFrequencyData(frequency); //周波数領域の波形データ取得
-
-for(var i = 0; i < bufferLength; i++) {
-  value = frequency[i]; // 波形データ 0 ~ 255が格納されている。
-}
-
-// (i * 44100 / 2048)Hzにてiの時の周波数が確定できる。
-*/
